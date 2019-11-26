0d9756631783a58672d3fa47c87bb108315c162c
Sandeep <sandeepchowdarymallineni007@gmail.com>
2019-11-26 11:21:02 +0530

    improvised commit

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,57 +1,53 @@
  <template>
   <div id="game">
     <section class="row">
-      <div class="players">
-        <div class="Asura">
-          <div class="small-6 columns">
-            <h1 class="text-center">YOU</h1>
+        <div class="small-6 columns">
+            <h1 class="text-center">ASURA</h1>
             <div class="healthbar">
-              <div
-                class="healthbar text-center"
-                style="background-color: green; margin: 0; color: white;"
-                :style="{width: playerHealth + '%'}"
-              >{{ playerHealth }}</div>
+                <div
+                        class="healthbar text-center"
+                        style="background-color: green; margin: 0; color: white;"
+                        :style="{width: playerHealth + '%'}">
+                    {{ playerHealth }}
+                </div>
             </div>
-          </div>
         </div>
-        <div class="Computer">
-          <div class="small-6 columns">
-            <h1 class="text-center">MONSTER</h1>
+        <div class="small-6 columns">
+            <h1 class="text-center">COMPUTER</h1>
             <div class="healthbar">
-              <div
-                class="healthbar text-center"
-                style="background-color: green; margin: 0; color: white;"
-                :style="{width: monsterHealth + '%'}"
-              >{{ monsterHealth }}</div>
+                <div
+                        class="healthbar text-center"
+                        style="background-color: green; margin: 0; color: white;"
+                        :style="{width: monsterHealth + '%'}">
+                    {{ monsterHealth }}
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </section>
+
     <section class="row controls" v-if="!gameIsRunning">
       <div class="small-12 columns">
-        <button id="start-game" @click="startGame" type="button" class="btn btn-success">START NEW GAME</button>
+        <button
+          id="start-game"
+          @click="startGame"
+          type="button"
+          class="btn btn-success"
+        >START NEW GAME</button>
       </div>
     </section>
     <section class="row controls" v-else>
       <div class="small-12 columns">
         <button id="attack" @click="attack" type="button" class="btn btn-primary">ATTACK</button>
-        
-        <button id="special-attack" @click="specialAttack" type="button" class="btn btn-info">SPECIAL ATTACK</button>
-        <button id="heal" @click="heal" type="button" class="btn btn-warning" >HEAL</button>
+        <button
+          id="special-attack"
+          @click="specialAttack"
+          type="button"
+          class="btn btn-info"
+        >SPECIAL ATTACK</button>
+        <button id="heal" @click="heal" type="button" class="btn btn-warning">HEAL</button>
         <button id="give-up" @click="giveUp" type="button" class="btn btn-danger">GIVE UP</button>
       </div>
     </section>
-    <!-- <section class="row log" v-if="turns.length > 0">
-      <div class="small-12 columns">
-        <ul>
-           <li
-            v-for="turn in turns"
-            :class="{'player-turn': turn.isPlayer, 'monster-turn': !turn.isPlayer}"
-          >{{ turn.text }}></li>-->
-        <!-- </ul>
-      </div>
-    </section> --> 
   </div>
 </template>
  
@@ -62,8 +58,7 @@ export default {
     return {
       playerHealth: 100,
       monsterHealth: 100,
-      gameIsRunning: false,
-      turns: []
+      gameIsRunning: false
     };
   },
   methods: {
@@ -71,15 +66,10 @@ export default {
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
-      this.turns = [];
     },
     attack: function() {
       let damage = this.calculateDamage(3, 10);
       this.monsterHealth -= damage;
-      this.turns.unshift({
-        isPlayer: true,
-        text: "Player hits Monster for " + damage
-      });
       if (this.checkWin()) {
         return;
       }
@@ -88,10 +78,6 @@ export default {
     specialAttack: function() {
       let damage = this.calculateDamage(10, 20);
       this.monsterHealth -= damage;
-      this.turns.unshift({
-        isPlayer: true,
-        text: "Player hits Monster hard for " + damage
-      });
       if (this.checkWin()) {
         return;
       }
@@ -103,10 +89,6 @@ export default {
       } else {
         this.playerHealth = 100;
       }
-      this.turns.unshift({
-        isPlayer: true,
-        text: "Player heals for 10"
-      });
       this.monsterAttacks();
     },
     giveUp: function() {
@@ -117,24 +99,20 @@ export default {
       let damage = this.calculateDamage(5, 12);
       this.playerHealth -= damage;
       this.checkWin();
-      this.turns.unshift({
-        isPlayer: false,
-        text: "Monster hits Player for " + damage
-      });
     },
     calculateDamage: function(min, max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min);
     },
     checkWin: function() {
       if (this.monsterHealth <= 0) {
-        if (confirm("You won! New Game?")) {
+        if (confirm("You won! Wanna play again?")) {
           this.startGame();
         } else {
           this.gameIsRunning = false;
         }
         return true;
       } else if (this.playerHealth <= 0) {
-        if (confirm("You lost! New Game?")) {
+        if (confirm("You lost! Wanna try again?")) {
           this.startGame();
         } else {
           this.gameIsRunning = false;
@@ -147,57 +125,59 @@ export default {
 };
 </script>
  
- <style scoped>
-.text-center {
-  text-align: center;
+<style scoped>
+* {
+  margin: 3% auto;
+}.text-center {
+    text-align: center;
 }
+
 .healthbar {
-  width: 70%;
-  height: 40px;
-  background-color: #eee;
-  transition: width 500ms;
+    width: 100%;
+    height: 40px;
+    background-color: #eee;
+    margin: auto;
+    transition: width 500ms;
 }
-.controls,
-.log {
-  margin-top: 30px;
-  text-align: center;
-  padding: 10px;
-  border: 1px solid #ccc;
-  box-shadow: 0px 3px 6px #ccc;
+
+.controls, .log {
+    margin-top: 30px;
+    text-align: center;
+    padding: 10px;
+    border: 1px solid #ccc;
+    box-shadow: 0px 3px 6px #ccc;
 }
+
 .turn {
-  margin-top: 20px;
-  margin-bottom: 20px;
-  font-weight: bold;
-  font-size: 22px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    font-weight: bold;
+    font-size: 22px;
 }
+
 .log ul {
-  list-style: none;
-  font-weight: bold;
-  text-transform: uppercase;
+    list-style: none;
+    font-weight: bold;
+    text-transform: uppercase;
 }
+
 .log ul li {
-  margin: 5px;
+    margin: 5px;
 }
+
 .log ul .player-turn {
-  color: blue;
-  background-color: #e4e8ff;
+    color: blue;
+    background-color: #e4e8ff;
 }
+
 .log ul .monster-turn {
-  color: red;
-  background-color: #ffc0c1;
+    color: red;
+    background-color: #ffc0c1;
 }
 
 button {
-  font-size: 20px;
-  
-  padding: 12px;
-  margin: 10px;
-}
-.players{
-    display: flex;
-}
-.Asura, .Computer{
-    flex:1;
+    font-size: 20px;
+    padding: 12px;
+    margin: 10px;
 }
 </style> 
